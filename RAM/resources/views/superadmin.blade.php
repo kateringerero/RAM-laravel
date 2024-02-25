3f54c1809f1ba481771fd0b4e70270c7cd10b59a
kateringerero <145843014+kateringerero@users.noreply.github.com>
2024-02-25 22:43:57 +0800

    Update superadmin.blade.php
    updated with tables

--- RAM/resources/views/superadmin.blade.php
+++ RAM/resources/views/superadmin.blade.php
@@ -1,13 +1,63 @@
 @extends('layout')
 
-@section('sidebar-options')
-    <li><a href="{{ route('manage-appointments') }}">Manage Appointments</a></li>
-    @if(auth()->check() && auth()->user()->role == 'superadmin')
-        <li><a href="/manage-admins">Manage Admins</a></li>
-    @endif
-    <li><a href="{{ route('my-account') }}">My Account/Profile</a></li>
-@endsection
-
 @section('content')
+<div class="appointments">
+    <div class="appointmentbox">
+        <h3></h3>
+        <div class="scrollable-table">
+        @if($schedules->isNotEmpty())
+            <div class="container-dashboard-appointments">
+                <h2>Appointments</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Creator ID</th>
+                            <th>Reference ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Scheduled Date</th>
+                            <th>Time</th>
+                            <th>Purpose</th>
+                            <th>Status</th>
+                            <th>Handled By</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($schedules as $schedule)
+                        <tr>
+                            <td>{{ $schedule->id }}</td>
+                            <td>{{ $schedule->creator_id }}</td>
+                            <td>{{ $schedule->reference_id }}</td>
+                            <td>{{ optional($schedule->user)->first_name }} {{ optional($schedule->user)->last_name }}</td>
+                            <td>{{ optional($schedule->user)->email_address }}</td>
+                            <td>{{ $schedule->scheduled_date }}</td>
+                            <td>{{ $schedule->start_time }} - {{ $schedule->end_time }}</td>
+                            <td>{{ $schedule->purpose }}</td>
+                            <td>{{ $schedule->status }}</td>
+                            <td>{{ optional($schedule->handler)->first_name }} {{ optional($schedule->handler)->last_name }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
 
+            </div>
+        @else
+            <p>No schedules found.</p>
+        @endif
+    </div>
+    </div>
+</div>
+
+<div class = "statistics">
+    <div class = "stats">
+        <p>releasing</p>
+    </div>
+    <div class = "stats">
+        <p>releasing</p>
+    </div>
+    <div class = "stats">
+        <p>approved</p>
+    </div>
+</div>
 @endsection
